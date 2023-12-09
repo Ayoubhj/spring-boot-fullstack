cc962d0e43c61a11908bf3f0a0dfb859b89b3bde
Ayoubhj <ayoub21hajouj@gmail.com>
2023-12-09 19:00:49 +0100

    uncomment command runner

--- backend/src/main/java/com/ayoubhj/SpringBootExampleApplication.java
+++ backend/src/main/java/com/ayoubhj/SpringBootExampleApplication.java
@@ -1,10 +1,14 @@
 package com.ayoubhj;
 
+import com.ayoubhj.customer.Customer;
 import com.ayoubhj.customer.CustomerRepository;
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
@@ -16,14 +20,14 @@ public class SpringBootExampleApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
 		return  args -> {
-			/*Faker faker = new Faker();
+			Faker faker = new Faker();
 			Random random = new Random();
 			String firstname = faker.name().firstName().toLowerCase();
 			String lastName = faker.name().lastName().toLowerCase();
 			Customer customer = new Customer( firstname + " " + lastName ,
 					firstname + "." + lastName +"@course.com",
 					random.nextInt(16,99));
-			customerRepository.save(customer);*/
+			customerRepository.save(customer);
 		};
 	}
 }
