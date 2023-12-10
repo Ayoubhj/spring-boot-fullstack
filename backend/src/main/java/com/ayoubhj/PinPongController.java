af7760e38b1260f01526e26d4515fb43624a0a50
Ayoubhj <ayoub21hajouj@gmail.com>
2023-12-10 19:52:02 +0100

    fix

--- backend/src/main/java/com/ayoubhj/PinPongController.java
+++ backend/src/main/java/com/ayoubhj/PinPongController.java
@@ -10,6 +10,6 @@ public class PinPongController {
 
     @GetMapping("ping")
     public PingPong getPingPong(){
-        return new PingPong("ayoub gdsgds");
+        return new PingPong("pong");
     }
 }
