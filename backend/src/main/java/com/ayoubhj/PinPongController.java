6fe8f91b3b561ab45e8ed2ab36b893c872bce67c
Ayoubhj <ayoub21hajouj@gmail.com>
2023-12-10 19:04:20 +0100

    CD Workflow with new controller

---
+++ backend/src/main/java/com/ayoubhj/PinPongController.java
@@ -0,0 +1,15 @@
+package com.ayoubhj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PinPongController {
+
+    record PingPong(String result){}
+
+    @GetMapping("ping")
+    public PingPong getPingPong(){
+        return new PingPong("Pong");
+    }
+}
